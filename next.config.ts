6c5133f66dfd8a6c3529b85c0048c50a734b351f
Adam Fehse <adamfehse@arizona.edu>
2025-07-02 13:00:41 -0700

    package

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repo = 'mapAppV2';
+
 const nextConfig: NextConfig = {
+  output: 'export',
+  assetPrefix: isProd ? `/${repo}/` : undefined,
+  basePath: isProd ? `/${repo}` : undefined,
   /* config options here */
 };
 
